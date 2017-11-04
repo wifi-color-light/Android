5488a90e51ef8259a850c128e78226605d60529d
LAPTOP-K9P8O50M\HP <494723324@qq.com>
2017-11-05 00:12:35 +0800

    创建wifi变色灯安卓控制端项目工程

---
+++ app/src/main/java/com/wifi_color_light/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wifi_color_light;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
