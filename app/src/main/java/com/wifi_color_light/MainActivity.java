dfec35b57b64eefdd59d983a2b4b1d84f43b8813
guihui <494723324@qq.com>
2017-11-06 21:49:18 +0800

    增加按键的触发实现，用于测试路由设置

--- app/src/main/java/com/wifi_color_light/MainActivity.java
+++ app/src/main/java/com/wifi_color_light/MainActivity.java
@@ -1,7 +1,14 @@
 package com.wifi_color_light;
 
+import android.net.wifi.WifiInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import static android.R.attr.button;
+import static android.R.attr.id;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bt = (Button)findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Configuration config = new Configuration(getApplicationContext());
+                Log.i("MainActivity", "onCreate: 已连接的wifi名称" + config.connectedWifiName());
+                if (config.setRouterInfo("360WiFi-D9B905","guihui1104.") == 0)
+                {
+                    Log.i("MainActivity", "onCreate: 配置路由器信息成功");
+                }else {
+                    Log.i("MainActivity", "onCreate: 配置路由器信息失败");
+                }
+            }
+        });
+
     }
 }
