ca96d92eaf2a03197f2df9cdb48b65b15c4cc002
guihui <494723324@qq.com>
2017-11-07 23:57:12 +0800

    更改测试逻辑代码

--- app/src/main/java/com/wifi_color_light/MainActivity.java
+++ app/src/main/java/com/wifi_color_light/MainActivity.java
@@ -1,35 +1,78 @@
 package com.wifi_color_light;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import static android.R.attr.button;
 import static android.R.attr.id;
 
 public class MainActivity extends AppCompatActivity {
-
+    private AlertDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bt = (Button)findViewById(R.id.button);
+        Button bt = (Button) findViewById(R.id.button);
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Configuration config = new Configuration(getApplicationContext());
-                Log.i("MainActivity", "onCreate: 已连接的wifi名称" + config.connectedWifiName());
-                if (config.setRouterInfo("360WiFi-D9B905","guihui1104.") == 0)
+                Configuration config = new Configuration(MainActivity.this);
+                String wifiName = config.connectedWifiName();
+                //showDialogTipUserRequestPermission();
+                Log.i("MainActivity", "onCreate: 已连接的wifi名称" + wifiName);
+                config.changeWifiConnected("guihui", "guihui00");
+
+                /*if (config.setRouterInfo(wifiName,"guihui1104.") == 0)
                 {
                     Log.i("MainActivity", "onCreate: 配置路由器信息成功");
                 }else {
                     Log.i("MainActivity", "onCreate: 配置路由器信息失败");
-                }
+                }*/
             }
         });
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                // 检查该权限是否已经获取
+                int i = ContextCompat.checkSelfPermission(this, permissions[0]);
+                // 权限是否已经 授权 GRANTED---授权  DINIED---拒绝
+                if (i != PackageManager.PERMISSION_GRANTED) {
+                    // 提示用户应该去应用设置界面手动开启权限
+                    new DynamicPermissions(MainActivity.this).showDialogTipUserGoToAppSettting(); ;
+                } else {
+                    if (dialog != null && dialog.isShowing()) {
+                        dialog.dismiss();
+                    }
+                    Toast.makeText(this, "权限获取成功", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
