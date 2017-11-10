9fb2b13062ce9ddd1391753d45cf8f2b0afcfbe6
Amzons <1159364870@qq.com>
2017-11-10 15:53:44 +0800

    1.删除了Configuration类，新增了WifiAdmin类进行WIFI的操作管理 2.废弃了老版本View绑定的繁琐步骤，改用butterknife来自动实现

--- app/src/main/java/com/wifi_color_light/MainActivity.java
+++ app/src/main/java/com/wifi_color_light/MainActivity.java
@@ -1,85 +1,76 @@
 package com.wifi_color_light;
 
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
 import android.os.Build;
-import android.provider.Settings;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
-import java.util.List;
-
-import static android.R.attr.button;
-import static android.R.attr.id;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.seekBar5)
+    SeekBar seekBar5;
+    @BindView(R.id.seekBar6)
+    SeekBar seekBar6;
+    @BindView(R.id.seekBar7)
+    SeekBar seekBar7;
     private AlertDialog dialog;
-    Button routerBt ;
-    EditText routerName;
-    EditText routerPass;
+    @BindView(R.id.routerNameText)
+    EditText routerNameText;
+    @BindView(R.id.routerPassText)
+    EditText routerPassText;
+    @BindView(R.id.router)
+    Button router;
+    @BindView(R.id.deviceAPname)
     EditText deviceAPname;
+    @BindView(R.id.deviceAPpass)
     EditText deviceAPpass;
-    Button configBt;
+    @BindView(R.id.scanBt)
     Button scanBt;
-    Configuration config;
+    @BindView(R.id.configBt)
+    Button configBt;
+    @BindView(R.id.button2)
+    Button button2;
+
+    private WifiAdmin mwifi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        routerBt = (Button) findViewById(R.id.router);
-        routerName = (EditText)findViewById(R.id.routerNameText);
-        routerPass = (EditText)findViewById(R.id.routerPassText);
-        deviceAPname = (EditText)findViewById(R.id.deviceAPname);
-        deviceAPpass = (EditText)findViewById(R.id.deviceAPpass);
-        configBt = (Button)findViewById(R.id.configBt) ;
-        scanBt = (Button)findViewById(R.id.scanBt);
-        config = new Configuration(MainActivity.this);
-        routerBt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        ButterKnife.bind(this);
+        mwifi = new WifiAdmin(this);
+    }
 
-                String wifiName = config.connectedWifiName();
-                routerName.setText(wifiName.substring(1,wifiName.length()-1));
-            }
-        });
-        scanBt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                List<ScanResult> scanResults = config.scanAccessPoint();
-                deviceAPname.setText("LAPTOP-K9P8O50M");
-                deviceAPpass.setText("01P7r9@5");
-            }
-        });
-        configBt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               if( config.changeWifiConnected(deviceAPname.getText().toString(),deviceAPpass.getText().toString()))
-               {
-
-                   //config.setRouterInfo(routerName.getText().toString(),routerPass.getText().toString());
-               }
-
-            }
-        });
-
-
-
+    @OnClick({R.id.router, R.id.scanBt, R.id.configBt})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.router:
+                routerNameText.setText(mwifi.getSSID());
+                Log.e("TAG", "onClick: ssid=" + mwifi.getSSID());
+                break;
+            case R.id.scanBt:
+                deviceAPname.setText("Mywifi");
+                deviceAPpass.setText("cyd123456");
+                break;
+            case R.id.configBt:
+                mwifi.disconnectWifi(mwifi.getNetworkId());
+                mwifi.addNetwork(mwifi.CreateWifiInfo("MyWifi", "cyd123456", 3));
+                break;
+        }
     }
 
     @Override
@@ -87,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == 1) {
 
-            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 // 检查该权限是否已经获取
                 int i = ContextCompat.checkSelfPermission(this, permissions[0]);
                 // 权限是否已经 授权 GRANTED---授权  DINIED---拒绝
@@ -109,4 +100,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
     }
+
 }
